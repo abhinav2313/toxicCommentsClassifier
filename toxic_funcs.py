77e9ceb3c1ba9e2f019babb3d4486005fab6202c
Nick Steinhauser <nicksteinhauser@MacBook-Air.local>
2018-03-27 19:31:36 -0500

    toxic_funcs custom funcs, etc

---
+++ toxic_funcs.py
@@ -0,0 +1,15 @@
+def Cleaner(text):
+    stops = ['\n', '...', '!', '?']
+    end_text = text
+    for stop in stops:
+        end_text = end_text.replace(stop, '. ')
+    
+    junks = "-\'=$%@#^,[]/><:;+"
+    for junk in junks:
+        end_text = end_text.replace(junk, '')
+    
+    regexs = ['http*htm.', ] #
+    for regex in regexs:
+        pass
+    
+    return end_text
