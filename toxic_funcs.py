c5c2162eaf7459ba2eb211ef8f4c2c2b8a2b3c85
Nick Steinhauser <nicksteinhauser@MacBook-Air.local>
2018-03-28 20:09:32 -0500

    cleaned up a little

--- toxic_funcs.py
+++ toxic_funcs.py
@@ -2,41 +2,51 @@ import re
 
 def Cleaner(input_text):
     
-    
+    #period placeholders
     stops = ['\n', '!', '?']
     
     output_text = input_text
     for stop in stops:
         output_text = output_text.replace(stop, '. ')
     
+    
+    #symbols to be removed
     junks = "-\'=$%@#^,[]/><+—`~"
     for junk in junks:
         output_text = output_text.replace(junk, '')
-    
     output_text = output_text.replace('"', '')
     
+    
+    #lower for further matching
     output_text = output_text.lower()
     
+    
+    #@TODO: add curse counter before further cleaning
+    
+    
+    #simple words that have no bearing on analysis
     #march/may is minorly problematic
     words = ['january','february','march','april',' may ','june','july','august','september','october','november','december', '(utc)', 'contribs', '(talk)']
     for word in words:
         output_text = output_text.replace(word, '')
-    #problem with regex: "asshole.jpg" would pass
+
     
-    regexs01 = ['http.*?(\s|$)', '(\d+[.])+\d+', '(\S*)\s*$', '\d+:\d+'] 
-    #urls, ip addresses or floats, parentheses at end of string, times ##:##, 
+    #easy regex replacements
+    regexs01 = ['http.*?(\s|$)', '(\d+[.])+\d+', '(\S*)\s*$', '\d+:\d+', '\d+'] 
+    #urls, ip addresses or floats, parentheses at end of string, times ##:##, nums
     
     for regex in regexs01:
         output_text, _ = re.subn(r''+ regex, '', output_text)
+    
     
     #regexs that are not simple replacements
     regexs02 = [ '(?:\w)(:|;)', '[(]\S\S', '\S\S[)]', '([.]\s*)+[.]'] 
     
     output_text, _ = re.subn(r'([.]\s*)+[.]', '.', output_text) #grouped periods
-    #output_text, _ = re.subn(r'(?:\w)(:|;)', '', output_text) #text: -> text
 
     
     #remove filenames, "youreanasshole.jpg" would pass without triggering net
+    #could be replaced by \s\S+?[.]\w\w\w\b
     options = '('
     extensions = ['jpg','zip','jpeg','png','tar','mpg','jpeg', 'svg']
     for extension in extensions:
