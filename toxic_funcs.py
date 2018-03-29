be19c86566be8c9a58c0c91e47d788a8bbde47fc
Nick Steinhauser <nicksteinhauser@MacBook-Air.local>
2018-03-29 13:09:55 -0500

    improved cleaner, :40

--- toxic_funcs.py
+++ toxic_funcs.py
@@ -1,61 +1,86 @@
 import re
 
+import re
+
 def Cleaner(input_text):
     
+    #@TODO: add curse counter before further cleaning
+    #@TODO: address colons, semicolons, parens
+    
     #period placeholders
-    stops = ['\n', '!', '?']
+    stops = ['\n', '!', '?', '\r', '\t']
     
     output_text = input_text
     for stop in stops:
         output_text = output_text.replace(stop, '. ')
     
-    
-    #symbols to be removed
-    junks = "-\'=$%@#^,[]/><+—`~"
-    for junk in junks:
-        output_text = output_text.replace(junk, '')
-    output_text = output_text.replace('"', '')
-    
-    
     #lower for further matching
     output_text = output_text.lower()
     
-    
+    #@TODO: add smiley detector with replacement by 'smileyemote' or something, would allow simple symbol replacer
     #@TODO: add curse counter before further cleaning
+    #@TODO: address colons, semicolons, parens
+
+    
+    #symbols to be removed
+    junks = "\'$%@#^,[]/><`~|•" #might want - here
+    for junk in junks:
+        output_text = output_text.replace(junk, '')
+        
+    space_junks = "-=+—" #symbols that can/often form word dividers
+    for junk in space_junks:
+        output_text = output_text.replace(junk, ' ')
+        
+    output_text = output_text.replace('"', '')
+        
+
+    '''#alternative symbol rejector, slower for current number of junk symbols, scales better though
+    symbols_to_keep = ' ().:;'
+    new_string = ''
+    for char in output_text:
+        if char.isalpha():
+            new_string += char
+        elif char in symbols_to_keep:
+            new_string += char'''
     
     
     #simple words that have no bearing on analysis
     #march/may is minorly problematic
-    words = ['january','february','march','april',' may ','june','july','august','september','october','november','december', '(utc)', 'contribs', '(talk)']
+    words = ['january','february','march','april',' may ','june', ' jun ','july','august','september','october','november','december', '(utc)', 'contribs', '(talk)']
     for word in words:
         output_text = output_text.replace(word, '')
+    new_string = ''    
+    
 
     
-    #easy regex replacements
-    regexs01 = ['http.*?(\s|$)', '(\d+[.])+\d+', '(\S*)\s*$', '\d+:\d+', '\d+'] 
-    #urls, ip addresses or floats, parentheses at end of string, times ##:##, nums
+    #easy regex replacements 
+    regexs01 = ['http.*?(\s|$)', '(\d+[.])+\d+', '[(]\S*[)]\s*$', '\d+:\d+', '\d+'] 
+    #urls, ip addresses or floats, parentheses at end of string, times ##:##, nums, 
     
     for regex in regexs01:
         output_text, _ = re.subn(r''+ regex, '', output_text)
     
     
     #regexs that are not simple replacements
-    regexs02 = [ '(?:\w)(:|;)', '[(]\S\S', '\S\S[)]', '([.]\s*)+[.]'] 
+    regexs02 = [ '(?:\w)(:|;)', '[(]\S\S', '\S\S[)]'] 
     
-    output_text, _ = re.subn(r'([.]\s*)+[.]', '.', output_text) #grouped periods
+    output_text, _ = re.subn(r'([.]\s*)+[.]', '. ', output_text) #grouped periods
 
     
-    #remove filenames, "youreanasshole.jpg" would pass without triggering net
-    #could be replaced by \s\S+?[.]\w\w\w\b
+    '''    #removes specific filetypes, "youreanasshole.jpg" would pass without triggering net
     options = '('
-    extensions = ['jpg','zip','jpeg','png','tar','mpg','jpeg', 'svg']
+    extensions = ['jpg','zip','jpeg','png','tar','mpg', 'svg', 'ogv']
     for extension in extensions:
         options += extension + '|'
-    options = options[:-1]
+    options = options[:-1] #remove last |
     options += ')'
     
     search_string = '\s\S+?[.]' + options
     
-    output_text, _ = re.subn(r''+ search_string, '', output_text)
+    output_text, _ = re.subn(r''+ search_string, '', output_text)'''
+    
+    #alternate file replacer, might be too general
+    #can be stuck into regexs01
+    output_text, _ = re.subn(r'\b\S+?[.]\S\S\S\b', '', output_text)
         
     return output_text
