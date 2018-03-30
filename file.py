5101afd2158c780f91c35ff3dd9746a51b7d0d82
Erdem Balikci <mrbalikci@gmail.com>
2018-03-30 17:37:10 -0500

    updated

--- file.py
+++ file.py
@@ -1,24 +1,61 @@
-def Cleaner(text):
-    stops = ['\n', '...', '!', '?']
-    end_text = text
+import re
 
+def Cleaner(input_text):
+    
+    #period placeholders
+    stops = ['\n', '!', '?']
+    
+    output_text = input_text
     for stop in stops:
-        end_text = end_text.replace(stop, '. ')
+        output_text = output_text.replace(stop, '. ')
     
-    junks = "-\'=$%@#^,[]/><:;+"
-    nums="0123456789"
     
+    #symbols to be removed
+    junks = "-\'=$%@#^,[]/><+—`~"
     for junk in junks:
-        end_text = end_text.replace(junk, '')
+        output_text = output_text.replace(junk, '')
+    output_text = output_text.replace('"', '')
     
-    for num in nums:
-        end_text = end_text.replace(num, '')
+    
+    #lower for further matching
+    output_text = output_text.lower()
+    
+    
+    #@TODO: add curse counter before further cleaning
+    
+    
+    #simple words that have no bearing on analysis
+    #march/may is minorly problematic
+    words = ['january','february','march','april',' may ','june','july','august','september','october','november','december', '(utc)', 'contribs', '(talk)']
+    for word in words:
+        output_text = output_text.replace(word, '')
 
+    
+    #easy regex replacements
+    regexs01 = ['http.*?(\s|$)', '(\d+[.])+\d+', '(\S*)\s*$', '\d+:\d+', '\d+'] 
+    #urls, ip addresses or floats, parentheses at end of string, times ##:##, nums
+    
+    for regex in regexs01:
+        output_text, _ = re.subn(r''+ regex, '', output_text)
+    
+    
+    #regexs that are not simple replacements
+    regexs02 = [ '(?:\w)(:|;)', '[(]\S\S', '\S\S[)]', '([.]\s*)+[.]'] 
+    
+    output_text, _ = re.subn(r'([.]\s*)+[.]', '.', output_text) #grouped periods
 
-    end_text = end_text.replace('"', '')
     
-    regexs = ['http*htm.', ]
-    for regex in regexs:
-        pass
+    #remove filenames, "youreanasshole.jpg" would pass without triggering net
+    #could be replaced by \s\S+?[.]\w\w\w\b
+    options = '('
+    extensions = ['jpg','zip','jpeg','png','tar','mpg','jpeg', 'svg']
+    for extension in extensions:
+        options += extension + '|'
+    options = options[:-1]
+    options += ')'
     
-    return end_text
+    search_string = '\s\S+?[.]' + options
+    
+    output_text, _ = re.subn(r''+ search_string, '', output_text)
+        
+    return output_text
